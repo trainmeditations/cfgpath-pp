469bd298f2082249e617b14c37ce25f3e7dc7c4c
Shaun Bouckaert <shaun@train-meditations.com>
2016-10-28 13:36:50 +1000

    Added initial code files

---
+++ cfgpath.cpp
@@ -0,0 +1,8 @@
+/*
+ * This code is based off Adam Nielsen's cfgpath C header found at
+ * https://github.com/Malvineous/cfgpath
+ *
+ * Following his code being public domain I have licensed this using the
+ * unlicense http://unlicense.org/. Full license text is available in
+ * the LICENSE file
+ */
---
+++ cfgpath.hpp
@@ -0,0 +1,14 @@
+/*
+ * This code is based off Adam Nielsen's cfgpath C header found at
+ * https://github.com/Malvineous/cfgpath
+ *
+ * Following his code being public domain I have licensed this using the
+ * unlicense http://unlicense.org/. Full license text is available in
+ * the LICENSE file
+ */
+#ifndef CFGPATH_HPP
+#define CFGPATH_HPP
+
+
+
+#endif // CFGPATH_HPP
