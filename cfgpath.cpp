a0dfe9e699652091d040251bbd23cc5febbd386c
Shaun Bouckaert <shaun@train-meditations.com>
2016-10-30 01:51:05 +1000

    Stringstreams and path seperators
    Switch to using stringstreams in internal logic
    Replace some hard coded path seperators with constants

--- cfgpath.cpp
+++ cfgpath.cpp
@@ -9,8 +9,10 @@
 
 #include "cfgpath.hpp"
 
+#include <sstream>
 #include <stdexcept>
 #include <cstdlib>
+
 \
 #ifdef _WIN32
 #include <shlobj.h>
@@ -23,8 +25,10 @@ const char _pathSep = '\\';
 const char _pathSep = '/';
 #endif
 
+using std::stringstream;
+
 string get_standard_config_path() {
-    string cfgPath;
+    stringstream cfgPath;
     //Windows first, then Apple, then other *nixes
 #ifdef WIN32
     //using ansi windows for now
@@ -33,8 +37,8 @@ string get_standard_config_path() {
     if (!SUCCEEDED(SHGetFolderPathA(NULL, CSIDL_APPDATA, NULL, 0, _confPath))) {
         throw std::runtime_error("Unable to get standard config path from system");
     }
-    cfgPath=_confPath;
-    cfgPath+= '\\';
+    cfgPath<< _confPath;
+    cfgPath<< _pathSep;
 #elif defined(__APPLE__)
 #elif defined(__unix__)
     //Follow XDG Specification
@@ -45,19 +49,19 @@ string get_standard_config_path() {
         //XDG_CONFIG_HOME isn't set. USE $HOME/.config
         _confHome = getenv("HOME");
         if (!_confHome) throw std::runtime_error("Unable to find home directory");
-        cfgPath = _confHome;
-        cfgPath += '/';
-        cfgPath += ".config";
+        cfgPath << _confHome;
+        cfgPath << _pathSep;
+        cfgPath << ".config";
         if (mkdir(cfgPath.c_str(), 0700) != 0 && errno != EEXIST)
             throw std::runtime_error("Unable to create .config in user home");
     } else {
-        cfgPath=_confHome;
+        cfgPath << _confHome;
     }
-    cfgPath += '/';
+    cfgPath << _pathSep;
 #else
     throw std::logic_error("Incompatible OS");
 #endif
-    return cfgPath;
+    return cfgPath.str();
 }
 
 bool createDirectoryIfNotExist(const string& path) {
@@ -76,18 +80,20 @@ bool createFileIfNotExist(const string& path) {
 }
 
 string cfgpath::get_user_config_folder(const string& appname) {
-    string cfgPath = get_standard_config_path();
-    cfgPath += appname;
-    if (!createDirectoryIfNotExist(cfgPath))
+    stringstream cfgPath;
+    cfgPath << get_standard_config_path();
+    cfgPath << appname;
+    if (!createDirectoryIfNotExist(cfgPath.str()))
         throw std::runtime_error("Unable to create application config directory");
-    cfgPath += _pathSep;
-    return cfgPath;
+    cfgPath << _pathSep;
+    return cfgPath.str();
 }
 
 string cfgpath::get_user_config_file(const string& appname) {
-    string cfgPath = get_standard_config_path();
-    cfgPath += appname;
-    if (!createFileIfNotExist(cfgPath))
+    stringstream cfgPath;
+    cfgPath << get_standard_config_path();
+    cfgPath << appname;
+    if (!createFileIfNotExist(cfgPath.str()))
         throw std::runtime_error("Unable to create application config file");
-    return cfgPath;
+    return cfgPath.str();
 }
